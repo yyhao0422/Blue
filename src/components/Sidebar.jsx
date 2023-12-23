358aebee92fd814604f9ebd12c47c71646686917
Yong Yung Hao <yyhao0422@gmail.com>
2023-12-23 21:54:22 +0800

    fix sidebar stickyness

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,7 +9,11 @@ export default function Sidebar({ children }) {
   const [isExpended, setIsExpended] = useState(true);
 
   return (
-    <aside className={`h-screen ${isExpended ? "w-[300px]" : "w-[90px]"}`}>
+    <aside
+      className={`h-screen sticky top-0 ${
+        isExpended ? "w-[300px]" : "w-[90px]"
+      }`}
+    >
       <nav className="h-full flex flex-col bg-white border-r shadow-sm">
         <div className="p-4 pb-2 flex justify-between items-center">
           {/* Blue Logo */}
